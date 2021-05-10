211143cb5662ef26df092eb6be5ca789259020b0
redbrain <42494361+redbrain@users.noreply.github.com>
2021-05-10 19:34:47 -0400

    Initial file upload
    Don't feel like messing with real Git today

---
+++ index.js
@@ -0,0 +1,36 @@
+// opendl
+
+// require all libs
+const {spawn} = require('child_process');
+const express = require('express');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('ffmpeg-static');
+
+const app = express(); // initialize server
+app.use(express.urlencoded({extended:true})); // form data parsing
+
+app.get('/', (req, res) => res.sendFile('index.html',{root:__dirname})); // root sends html page
+
+app.post('/dl', async (req, res) => {
+    if (!ytdl.validateURL(req.body.url)) {
+        return res.status(422).send("Invalid YouTube link/id: "+req.body.url); // error if input incorrect
+    };
+    const info = await ytdl.getInfo(req.body.url);
+    if (req.body.type === "a") {
+        res.attachment(info.videoDetails.title+' (opendl).mp3'); // make browser treat as attachment
+        const audio = ytdl.downloadFromInfo(info,{quality:'highestaudio'});
+        const proc = spawn(ffmpeg, ['-loglevel', '8', '-i', 'pipe:3', '-f', 'mp3', 'pipe:4'], {stdio:['inherit','inherit','inherit','pipe','pipe']}); // convert to mp3
+        audio.pipe(proc.stdio[3]);
+        proc.stdio[4].pipe(res);
+    } else { // otherwise type must be v
+        res.attachment(info.videoDetails.title+' (opendl).mp4'); // make browser treat as attachment
+        const audio = ytdl.downloadFromInfo(info,{quality:'highestaudio'});
+        const video = ytdl.downloadFromInfo(info,{quality:'highestvideo',filter:f=>f.container==='mp4'});
+        const proc = spawn(ffmpeg, ['-loglevel', '8', '-i', 'pipe:3', '-i', 'pipe:4', '-map', '0:a', '-map', '1:v', '-vcodec', 'copy', '-movflags', 'frag_keyframe+empty_moov', '-f', 'mp4', 'pipe:5'], {stdio:['inherit','inherit','inherit','pipe','pipe','pipe']}); // convert to mp4
+        audio.pipe(proc.stdio[3]);
+        video.pipe(proc.stdio[4]);
+        proc.stdio[5].pipe(res);
+    };
+});
+
+const listener = app.listen(process.env.PORT || 8080 || 0, () => console.log("It's alive! at localhost:"+listener.address().port));
