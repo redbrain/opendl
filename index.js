f1474de32e521298250349405a66340f356e7d54
redbrain <42494361+redbrain@users.noreply.github.com>
2023-02-21 17:16:28 -0500

    Temporary(?) fix for video download not working properly

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.post('/dl', async (req, res) => {
         proc.stdio[4].pipe(res);
     } else { // otherwise type must be v
         res.attachment(info.videoDetails.title+' (opendl).mp4'); // make browser treat as attachment
-        const audio = ytdl.downloadFromInfo(info,{quality:'highestaudio',filter:f=>f.container==='m4a'});
-        const video = ytdl.downloadFromInfo(info,{quality:'highestvideo',filter:f=>f.container==='mp4'});
+        const audio = ytdl.downloadFromInfo(info,{quality:[141,140,139]});
+        const video = ytdl.downloadFromInfo(info,{quality:[138,299,298,264,137,136,135,134,133,160]});
         const proc = spawn(ffmpeg, ['-loglevel', '8', '-i', 'pipe:3', '-i', 'pipe:4', '-map', '0:a', '-map', '1:v', '-c', 'copy', '-movflags', 'frag_keyframe+empty_moov', '-f', 'mp4', 'pipe:5'], {stdio:['inherit','inherit','inherit','pipe','pipe','pipe']}); // convert to mp4
         audio.pipe(proc.stdio[3]);
         video.pipe(proc.stdio[4]);
